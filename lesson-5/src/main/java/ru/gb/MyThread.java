5263c462e82b3debb96670ac3b5d8da379b9577c
kornev <aakor23@gmail.com>
2020-07-14 12:50:00 +0300

    Lesson 5 added

--- lesson-5/src/main/java/ru/gb/MyThread.java
+++ lesson-5/src/main/java/ru/gb/MyThread.java
@@ -1,10 +1,18 @@
 package ru.gb;
 
 public class MyThread extends Thread {
+
+        private  float[] arr;
+
         @Override
         public void run() {
             System.out.println("New thread started");
         }
+
+        public void MyThread (float[] arr) {
+            System.arraycopy(this.arr, 0, arr.length, 0, arr.length);
+        }
+
 
         public float[] calc (float[] arr) {
             float[] arrReturn = new float[arr.length];
